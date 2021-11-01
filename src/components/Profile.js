47b7e654b85521fcff9b15ff941b7ca332befd8b
João Costa <costav1402@gmail.com>
2021-11-01 17:20:22 -0300

    create profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,33 @@
 import React from "react";
-import Form from "react-validation/build/form";
-import Input from "react-validation/build/input";
-import CheckButton from "react-validation/build/button";
+import AuthService from "../services/auth.service";
 
-import { isEmail } from "validator";
+const Profile = () => {
+  const currentUser = AuthService.getCurrentUser();
+
+  return (
+    <div className="container">
+      <header className="jumbotron">
+        <h3>
+          <strong>{currentUser.username}</strong> Profile
+        </h3>
+      </header>
+      <p>
+        <strong>Token:</strong> {currentUser.accessToken.substring(0, 20)} ...{" "}
+        {currentUser.accessToken.substr(currentUser.accessToken.length - 20)}
+      </p>
+      <p>
+        <strong>Id:</strong> {currentUser.id}
+      </p>
+      <p>
+        <strong>Email:</strong> {currentUser.email}
+      </p>
+      <strong>Authorities:</strong>
+      <ul>
+        {currentUser.roles &&
+          currentUser.roles.map((role, index) => <li key={index}>{role}</li>)}
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
